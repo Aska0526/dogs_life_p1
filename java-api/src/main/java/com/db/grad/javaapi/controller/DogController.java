f6f4d1572bf745cb5e39966167b7cd476d6de73f
Chao Fan <chaoaskafan@gmail.com>
2023-07-28 16:28:10 +0100

    implemented find dogs by id

--- java-api/src/main/java/com/db/grad/javaapi/controller/DogController.java
+++ java-api/src/main/java/com/db/grad/javaapi/controller/DogController.java
@@ -3,6 +3,10 @@ package com.db.grad.javaapi.controller;
 import com.db.grad.javaapi.service.DogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.db.grad.javaapi.model.Dog;
+import com.db.grad.javaapi.service.DogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +14,11 @@ public class DogController {
 
     @Autowired
     DogService dogService;
+
+    @GetMapping("/dogs/{id}")
+    public Dog getDogBySpecificID(long id){
+        return dogService.getDogById(id);
+    }
 
     @GetMapping
     public long getNoOfDogsInDb(){
