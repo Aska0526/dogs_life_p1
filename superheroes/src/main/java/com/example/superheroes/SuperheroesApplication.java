d4d18a1df8ff2a5843a920c02f352f68ff16a2e5
F1GYGA <alexandru.hrenciuc12@gmail.com>
2023-07-28 11:32:01 +0100

    qla 3 and 4

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -1,11 +1,23 @@
 package com.example.superheroes;
 
+import com.example.superheroes.model.Hero;
+import com.example.superheroes.practice.Calculator;
+import com.example.superheroes.repository.HeroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class SuperheroesApplication implements CommandLineRunner {
+
+	@Autowired
+	Calculator calc;
+
+	@Autowired
+	HeroRepository heroRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroesApplication.class, args);
@@ -13,6 +25,23 @@ public class SuperheroesApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Helau world");
+		List<Hero> heroList = heroRepository.findAll();
+		for(Hero hero: heroList){
+			System.out.println(hero);
+		}
+
+		System.out.println();
+
+		heroList = heroRepository.findTop3ByOrderByNameDesc();
+		for(Hero hero: heroList){
+			System.out.println(hero);
+		}
+
+		System.out.println();
+
+		heroList = heroRepository.findHeroesNameStartingWithLetter("C%");
+		for(Hero hero: heroList){
+			System.out.println(hero);
+		}
 	}
 }
