bcd6d7de2bfae943208d36d8249ab6bbe6c1f99f
Chao Fan <chaoaskafan@gmail.com>
2023-07-31 18:52:44 +0100

    react exercise

---
+++ ReactPractice/superheroes-ui/src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import logo from './logo.svg';
+import './App.css';
+import AllHeroes from './components/AllHeroes';
+
+function App() {
+  return (
+    <AllHeroes/>
+  );
+}
+
+export default App;
