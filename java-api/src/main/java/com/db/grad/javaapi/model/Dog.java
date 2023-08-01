8a35cb1430bf10c93e4e652bfc89e6e0019294c5
Ruslan Salkaev <rsalkaev@gmail.com>
2023-08-01 14:58:37 +0100

    Add owner DAO

--- java-api/src/main/java/com/db/grad/javaapi/model/Dog.java
+++ java-api/src/main/java/com/db/grad/javaapi/model/Dog.java
@@ -1,16 +1,49 @@
 package com.db.grad.javaapi.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
-@Table(name="dogs")
-public class Dog
-{
+@Table(name = "dogs")
+public class Dog {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "age")
+    private int age;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "owner_id", referencedColumnName = "id")
+    private Owner owner;
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", owner=" + owner +
+                '}';
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     public long getId() {
         return id;
