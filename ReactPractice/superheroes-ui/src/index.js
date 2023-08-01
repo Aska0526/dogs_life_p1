985728b78673b0f09fa5c37608295e911a7895e1
unknown <alexandru.hrenciuc12@gmai.com>
2023-08-01 10:59:26 +0100

    ql5 6 7 8

--- ReactPractice/superheroes-ui/src/index.js
+++ ReactPractice/superheroes-ui/src/index.js
@@ -13,6 +13,7 @@ import Deadshot from "./images/7.jpeg";
 import CountVertigo from "./images/8.jpeg";
 import Robin from "./images/9.jpeg";
 import Nightwing from "./images/10.jpeg";
+import { BrowserRouter } from 'react-router-dom';
 
 const images = {
     Batman,
@@ -28,11 +29,15 @@ const images = {
 }
 export default images;
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
