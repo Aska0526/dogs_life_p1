aeae5c16e1d92cf25417ecd5b41d9d6e41ba6c94
unknown <alexandru.hrenciuc12@gmai.com>
2023-08-01 15:45:02 +0100

    created dog life fe

---
+++ react-app/react-app-doggs-ui/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import logo from './logo.svg';
+import './App.css';
+import DogDetail from './components/DogDetail';
+
+
+function App() {
+  return (
+    <DogDetail/>
+  );
+}
+
+export default App;
---
+++ react-app/react-app-doggs-ui/src/components/AllDogs.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import DogDetail from './DogDetail'
+import { useState } from 'react';
+import { getAllDogs, getNoOfDogs } from '../services/dog-service';
+import { Row } from 'react-bootstrap';
+const AllDogs = () => {
+
+
+    const [dogs, setDogs] = useState([]);
+    const [dogCount, setDogCount] = useState(0);
+
+    useEffect(() => {
+        getDogsFromAPI();
+        getNoOfDogsFromAPI();
+    }, []);
+
+    const getDogsFromAPI = () => {
+        getAllDogs()
+            .then(res => {
+                setDogs(res.data);
+            })
+            .catch(err => {
+                setDogs([]);
+                console.log(err);
+            })
+
+    };
+
+    const getNoOfDogsFromAPI = () => {
+        getNoOfDogs()
+            .then(res => {
+                setDogCount(res.data);
+            })
+            .catch(err => {
+                setDogCount(0);
+                console.log(err);
+            })
+    }
+
+    return (
+        <>
+         <h1>Total count of dogs is: {dogCount}</h1>
+        <Row>
+            {dogs.map(dog => (
+                <div key={dog.id.toString()}>
+                    <DogDetail info={dog}/>
+                </div>
+            ))}
+        </Row>
+       
+        
+        </>
+    )
+}
+
+export default AllDogs
